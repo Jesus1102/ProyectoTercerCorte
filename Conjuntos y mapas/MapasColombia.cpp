0aa9afcbd3ab55e6190bbb7179fd894eccd0464c
Jesus1102 <jesus60302@gmail.com>
2024-04-23 21:31:06 -0500

    Update

--- Conjuntos y mapas/MapasColombia.cpp
+++ Conjuntos y mapas/MapasColombia.cpp
@@ -1,74 +1,59 @@
 #include <iostream>
 #include <map>
+#include <vector>
 using namespace std;
-
-string capital();
-string clima();
 
 int main(){
 
-    map<string, string> departamentos;
-    departamentos["Amazonas"]="Leticia";
-    departamentos["Antioquia"]="Medellín";
-    departamentos["Arauca"]="Arauca";
-    departamentos["Atlántico"]="Barranquilla";
-    departamentos["Bolívar"]="Cartagena";
-    departamentos["Boyacá"]="Tunja";
-    departamentos["Caldas"]="Manizales";
-    departamentos["Caquetá"]="Florencia";
-    departamentos["Casanare"]="Yopal";
-    departamentos["Cauca"]="Popayán";
-    departamentos["Cesar"]="Valledupar";
-    departamentos["Chocó"]="Quibdó";
-    departamentos["Córdoba"]="Montería";
-    departamentos["Cundinamarca"]="Bogotá";
-    departamentos["Guainía"]="Inírida";
-    departamentos["Guaviare"]="San José del Guaviare";
-    departamentos["Huila"]="Neiva";
-    departamentos["La Guajira"]="Riohacha";
-    departamentos["Magdalena"]="Santa Marta";
-    departamentos["Meta"]="Villavicencio";
-    departamentos["Nariño"]="Pasto";
-    departamentos["Norte de Santander"]="Cúcuta";
-    departamentos["Putumayo"]="Mocoa";
-    departamentos["Quindío"]="Armenia";
-    departamentos["Risaralda"]="Pereira";
-    departamentos["San Andrés y Providencia"]="San Andrés";
-    departamentos["Santander"]="Bucaramanga";
-    departamentos["Sucre"]="Sincelejo";
-    departamentos["Tolima"]="Ibagué";
-    departamentos["Valle del Cauca"]="Cali";
-    departamentos["Vaupés"]="Mitú";
-    departamentos["Vichada"]="Puerto Carreño";
+    map<string, map<string, string>> departamentos;
+    departamentos["Amazonas"].insert(pair<string, string>("Leticia", "Caluroso"));
+    departamentos["Antioquia"].insert(pair<string, string>("Medellín", "Moderado"));
+    departamentos["Arauca"].insert(pair<string, string>("Arauca", "Caluroso"));
+    departamentos["Atlántico"].insert(pair<string, string>("Barranquilla", "Caluroso"));
+    departamentos["Bolívar"].insert(pair<string, string>("Cartagena", "Caluroso"));
+    departamentos["Boyacá"].insert(pair<string, string>("Tunja", "Frío"));
+    departamentos["Caldas"].insert(pair<string, string>("Manizales", "Moderado"));
+    departamentos["Caquetá"].insert(pair<string, string>("Florencia", "Caluroso"));
+    departamentos["Casanare"].insert(pair<string, string>("Yopal", "Caluroso"));
+    departamentos["Cauca"].insert(pair<string, string>("Popayán", "Moderado"));
+    departamentos["Cesar"].insert(pair<string, string>("Valledupar", "Caluroso"));
+    departamentos["Chocó"].insert(pair<string, string>("Quibdó", "Caluroso"));
+    departamentos["Córdoba"].insert(pair<string, string>("Montería", "Caluroso"));
+    departamentos["Cundinamarca"].insert(pair<string, string>("Bogotá", "Frío"));
+    departamentos["Guainía"].insert(pair<string, string>("Inírida", "Caluroso"));
+    departamentos["Guaviare"].insert(pair<string, string>("San José del Guaviare", "Caluroso"));
+    departamentos["Huila"].insert(pair<string, string>("Neiva", "Caluroso"));
+    departamentos["La Guajira"].insert(pair<string, string>("Riohacha", "Caluroso"));
+    departamentos["Magdalena"].insert(pair<string, string>("Santa Marta", "Caluroso"));
+    departamentos["Meta"].insert(pair<string, string>("Villavicencio", "Caluroso"));
+    departamentos["Nariño"].insert(pair<string, string>("Pasto", "Moderado"));
+    departamentos["Norte de Santander"].insert(pair<string, string>("Cúcuta", "Caluroso"));
+    departamentos["Putumayo"].insert(pair<string, string>("Mocoa", "Caluroso"));
+    departamentos["Quindío"].insert(pair<string, string>("Armenia", "Moderado"));
+    departamentos["Risaralda"].insert(pair<string, string>("Pereira", "Moderado"));
+    departamentos["San Andrés y Providencia"].insert(pair<string, string>("San Andrés", "Caluroso"));
+    departamentos["Santander"].insert(pair<string, string>("Bucaramanga", "Moderado"));
+    departamentos["Sucre"].insert(pair<string, string>("Sincelejo", "Caluroso"));
+    departamentos["Tolima"].insert(pair<string, string>("Ibagué", "Caluroso"));
+    departamentos["Valle del Cauca"].insert(pair<string, string>("Cali", "Caluroso"));
+    departamentos["Vaupés"].insert(pair<string, string>("Mitú", "Caluroso"));
+    departamentos["Vichada"].insert(pair<string, string>("Puerto Carreño", "Caluroso"));
 
-    for (const auto& par:departamentos)
-    {
-        string clave = par.first;
-        string valor = par.second;
-        cout<<"Departamento:"<<clave<<" Capital:"<<valor<<endl;
-    }
 
+    string departamento;
+    vector <string> ciudades;
+
+    cout << "Ingrese el departamento: ";
+    cin >> departamento;
+    auto it = departamentos.find(departamento);
+    if(it != departamentos.end()){
+        cout << "Capital: " << it->second.begin()->first << endl;
+        cout << "Clima: " << it->second.begin()->second << endl;
 
     return 0;
 }
 
 
-string capital(map <string, string> departamentos){ 
-
-    string departamento;
-    cout<<"Ingrese el nombre del departamento: ";
-    cin>>departamento;
-
-    auto it=departamentos.find(departamento);
-
-    if (it!=departamentos.end())
-    {
-        cout << "La capital de "<<departamento<<" es "<<departamentos[departamento];
-    }else{
-        cout << "No se encontró el departamento "<<departamento;
-    }
 
 
-
-}
 
