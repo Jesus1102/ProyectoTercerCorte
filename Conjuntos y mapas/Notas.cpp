f2a3cdf18d865affa2b416cd76092d88787b3eed
Jesus1102 <jesus60302@gmail.com>
2024-04-30 10:44:42 -0500

    Update

--- Conjuntos y mapas/Notas.cpp
+++ Conjuntos y mapas/Notas.cpp
@@ -7,6 +7,25 @@ int main(){
 
     map <string, map<string, float>> notas;
 
+    string nombre, asignatura;
+    float nota;
+
+    
+    while(nombre != "fin"){
+        cout << "Ingrese el nombre del estudiante:  (fin para salir) ";
+        cin >> nombre;
+        cout << "Ingrese la asignatura: ";
+        cin >> asignatura;
+        cout << "Ingrese la nota: ";
+        cin >> nota;
+
+        if (nombre != "fin")
+        {
+            notas[nombre].insert(pair<string, float>(asignatura, nota));
+        }
+    
+    }
+
 
 
 
