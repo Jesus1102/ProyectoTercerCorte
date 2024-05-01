fa4671e0a9152164dbc272eee9cb25aafdd84419
Jesus1102 <jesus60302@gmail.com>
2024-05-01 10:47:02 -0500

    Cree y termine llamadas.cpp

---
+++ Pilas y Colas/Pilas/Llamadas.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+
+struct Llamadas
+{
+    string numero;
+    string duracion;
+};
+
+
+int main(){
+
+    stack<Llamadas> LlamadasTelefonicas;
+
+    Llamadas LlamadasT;
+    while(true){
+
+        cout << "Ingrese el numero de la llamada: (0. para salir) " << endl;
+        cin >> LlamadasT.numero;
+        if(LlamadasT.numero == "0"){
+            break;
+        }
+        else{
+
+            cout << "Ingrese la duracion de la llamada(minutos:segundos): " << endl;
+            cin >> LlamadasT.duracion;
+            LlamadasTelefonicas.push(LlamadasT);
+        }
+
+
+    }
+    
+}
