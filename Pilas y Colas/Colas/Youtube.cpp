cbcd9c6d6feb1ea8ba9ec4397bb3a44d9356037a
Jesus1102 <jesus60302@gmail.com>
2024-05-01 11:40:17 -0500

    Update Youtube.cpp

--- Pilas y Colas/Colas/Youtube.cpp
+++ Pilas y Colas/Colas/Youtube.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main(){
 
     queue<string> colaReproduccion;
-    int turno = 1, cedula, opcion;
+    string cancion;
+    int opcion;
 
 
     while(true){
@@ -15,8 +16,29 @@ int main(){
         cout << "1. para registra una cancion" << endl;
         cout << "2. para ver la lista de canciones" << endl;
         cout << "0. para salir" << endl;
-
         cin >> opcion;
+
+        if(opcion == 1){
+            cout << "Ingrese el nombre de la cancion:" << endl;
+            cin >> cancion;
+            colaReproduccion.push(cancion);
+        }
+        else if(opcion == 2){
+            
+            while(!colaReproduccion.empty()){
+                cout << colaReproduccion.front() << endl;
+                colaReproduccion.pop();
+            }
+            if(colaReproduccion.empty()){
+                cout << "No hay canciones registradas" << endl;
+            }
+        }
+        else if(opcion == 0){
+            break;
+        }       
+        else{
+            cout << "Opcion invalida" << endl;
+        }
     }
     return 0;
 }
